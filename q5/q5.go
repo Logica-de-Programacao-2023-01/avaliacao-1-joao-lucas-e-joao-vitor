05e33c1c598f18535f03f1f9dc718e0396b97558
joaovitortm4 <126472631+joaovitortm4@users.noreply.github.com>
2023-04-24 11:11:33 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,6 +1,6 @@
 package q5
 
-import "fmt"
+import ("fmt" "strings")
 
 //Pedro começou a frequentar aulas de programação. Na primeira aula, sua tarefa foi escrever um programa simples. O
 //programa deveria fazer o seguinte: na sequência de caracteres fornecida, composta por letras latinas maiúsculas e
